b23bfe6afb68bd44e405f65347f7f2712054fac1
marious <mohamedraya410@gmail.com>
2022-04-17 13:33:08 +0200

    first commit

--- components/auth/sign-up-form.jsx
+++ components/auth/sign-up-form.jsx
@@ -2,16 +2,22 @@ import { useAtom } from 'jotai';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'react-i18next';
 import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import ALink from '~/components/features/alink';
 import { useRegisterMutation } from '~/framework/rest/auth/auth.query';
 import { authorizationAtom } from '~/store/authorization-atom';
+import { useForm } from 'react-hook-form';
+import Cookies from 'js-cookie';
+import { AUTH_TOKEN } from '../../utils/constants';
 
 const registerFormSchema = yup.object().shape({
+    name: yup.string().required(),
     email: yup.string().email().required(),
     password: yup.string().required(),
 });
 
 const defaultValues = {
+    name: '',
     email: '',
     password: '',
 };
@@ -23,9 +29,52 @@ export default function SignUpForm() {
     const [_, authorize] = useAtom(authorizationAtom);
     const { mutate: signUp, isLoading } = useRegisterMutation();
 
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(registerFormSchema),
+        defaultValues,
+    });
+
+    function onSubmit({ name, email, password }) {
+        signUp(
+            {
+                name,
+                email,
+                password,
+            },
+            {
+                onSuccess: data => {
+                    if (data?.token) {
+                        Cookies.set(AUTH_TOKEN, data.token);
+                        authorize(true);
+                    }
+                    if (!data.token) {
+                        setErrorMessage('Error credential wrong');
+                    }
+                },
+                onError: error => {
+                    console.log(error);
+                },
+            }
+        );
+    }
+
     return (
         <>
             <form action="#">
+                <div className="form-group">
+                    <label htmlFor="register-email-2">Your Name *</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        {...register('name')}
+                    />
+                </div>
+
                 <div className="form-group">
                     <label htmlFor="register-email-2">
                         Your email address *
@@ -35,7 +84,7 @@ export default function SignUpForm() {
                         className="form-control"
                         id="register-email-2"
                         name="register-email"
-                        required
+                        {...register('email')}
                     />
                 </div>
 
@@ -46,7 +95,7 @@ export default function SignUpForm() {
                         className="form-control"
                         id="register-password-2"
                         name="register-password"
-                        required
+                        {...register('password')}
                     />
                 </div>
 
